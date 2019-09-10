5cec70b75ebd2a9af8488fbe13bbf0598dd40784
Lukas Ferreira <49461720+lukasferreira@users.noreply.github.com>
2019-09-10 11:26:20 -0300

    Update desafio_1.java

--- desafio_1.java
+++ desafio_1.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class desafio_1 {
 
 	public static void main(String[] args) {
-		//Crie um programa em JAVA que peça um número ao usuário e armazene ele na variável x. Depois peça outro número e armazene na variável y.
-		//Mostre esses números. Em seguida, faça com que x passe a ter o valor de y, e que y passe a ter o valor de x.
-		//Dica: você vai precisar usar outra variável. Você NÃO precisa trabalhar com o comando SE.
+		//Crie um programa em JAVA que peÃ§a um nÃºmero ao usuÃ¡rio e armazene ele na variÃ¡velÂ x. Depois peÃ§a outro nÃºmero e armazene na variÃ¡velÂ y.
+		//Mostre esses nÃºmeros. Em seguida, faÃ§a com queÂ xÂ passe a ter o valor deÂ y, e queÂ yÂ passe a ter o valor deÂ x.
+		//Dica: vocÃª vai precisar usar outra variÃ¡vel. VocÃª NÃƒO precisa trabalhar com o comando SE.
 		int x = 0;
 		int y = 0;
 		int z = 0;
@@ -18,7 +18,7 @@ public class desafio_1 {
 		x = teclado.nextInt();
 		
 		teclado = null;
-	    teclado = new Scanner(System.in);
+	   	teclado = new Scanner(System.in);
 	    
 		System.out.println("Insira um valor para Y: ");
 		y = teclado.nextInt();
