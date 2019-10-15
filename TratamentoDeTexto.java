82cb4a40e67bcb827d31909ec119cb0c00246ce7
Lukas Ferreira <49461720+lukasferreira@users.noreply.github.com>
2019-10-15 16:34:21 -0300

    Update TratamentoDeTexto.java

--- TratamentoDeTexto.java
+++ TratamentoDeTexto.java
@@ -14,9 +14,9 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 public class resenhasF {
-	
+
 	public static void main(String[] args) {
-		
+
 		try {
 			FileReader stop = new FileReader("C:\\Users\\Lord\\Desktop\\Faculdade\\stopwords.txt");
 			BufferedReader stop_reader = new BufferedReader(stop);
@@ -26,12 +26,12 @@ public class resenhasF {
 				stop_list.add(stop_palavra);
 				stop_palavra = stop_reader.readLine();
 			}
-			
+
 			FileReader corpus = new FileReader("C:\\Users\\Lord\\Desktop\\Faculdade\\corpus.txt");
 			BufferedReader corpus_reader = new BufferedReader(corpus);
 			String linha = corpus_reader.readLine();
 			String[] s;
-			
+
 			FileReader qualidades = new FileReader("C:\\Users\\Lord\\Desktop\\Faculdade\\qualidades.txt");
 			BufferedReader qualidades_reader = new BufferedReader(qualidades);
 			ArrayList<String> qualidades_list = new ArrayList<String>();
@@ -40,7 +40,7 @@ public class resenhasF {
 				qualidades_list.add(filter_qualidades);
 				filter_qualidades = qualidades_reader.readLine();
 			}
-			
+
 			FileReader defeitos = new FileReader("C:\\Users\\Lord\\Desktop\\Faculdade\\defeitos.txt");
 			BufferedReader defeitos_reader = new BufferedReader(defeitos);
 			ArrayList<String>defeitos_list = new ArrayList<String>();
@@ -49,17 +49,17 @@ public class resenhasF {
 				defeitos_list.add(filter_defeitos);
 				filter_defeitos = defeitos_reader.readLine();	
 			}
-			
+
 			HashMap<String, Integer> mapa_de_palavras = new HashMap<String, Integer>();
 			int totalStop = 0, total=0, totQualidades = 0, totDefeitos = 0;
 			while(linha != null) {
 				s = linha.toLowerCase().split(" ");
-			
+
 				for(String palavra : s) {
 					total+=1;
 					if(!stop_list.contains(palavra)) {
 						totalStop +=1;
-	
+
 						if(qualidades_list.contains(palavra)) {
 							totQualidades += 1;	
 						}
@@ -76,17 +76,17 @@ public class resenhasF {
 					}
 				}
 				linha = corpus_reader.readLine();
-		
-		for(Entry<String, Integer> entrada : mapa_de_palavras.entrySet()) {
-			System.out.println(entrada.getKey() + " = " + entrada.getValue());
-		}
-		}
+
+				for(Entry<String, Integer> entrada : mapa_de_palavras.entrySet()) {
+					System.out.println(entrada.getKey() + " = " + entrada.getValue());
+				}
+			}
 			System.out.println("O total de palavras (com stopwords): "+totalStop);
 			System.out.println("O total de palavras em geral: "+ total);
 			System.out.println("Quantidade de adjetivos positivos: " + totQualidades);
 			System.out.println("Quantidade de adjetivos negativos: " + totDefeitos);
-		stop_reader.close();
-		corpus_reader.close();
+			stop_reader.close();
+			corpus_reader.close();
 		}	
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
